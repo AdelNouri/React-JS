d11530e2148294a65bafc85839e310d030068e69
Adel Nouri <adelnouri0231@gmail.com>
2024-09-27 12:19:16 -0700

    Update project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,28 @@ import Hello from './hello.js'
 import './App.css';
 
 function App() {
+  const me = 'Adel'
+  const users = [
+     {
+      firstname: 'Mohammad Reza'
+     },
+     {
+      firstname: 'Ali'
+     }, 
+     {
+      firstname: 'Mufokoli'
+     }
+  ]
+
   return (
     <div className="App">
       <Hello/>
+      {me? <h1>Hello {me}</h1> : <h1>Hello User</h1>}
+      
+      {users.map(user => (
+        <h1>{user.firstname}</h1>
+      ))}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
