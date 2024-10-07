0f03058fed9729bad20ae825f0ff2c0b9abf559a
Adel Nouri <adelnouri0231@gmail.com>
2024-10-07 22:43:19 +0330

    add contact card

--- contact-manager-app/src/components/contact/Contacts.jsx
+++ contact-manager-app/src/components/contact/Contacts.jsx
@@ -1,11 +1,30 @@
-import { Fragment } from 'react'
+import Contact from "./Contact";
+import { PINK } from "../../helpers/colors";
 
 const Contacts = () => {
-    return (
-        <Fragment>
-            <h3>Contact</h3>
-        </Fragment>
-    )
-}
+  return (
+    <>
+      <section className="container">
+        <div className="grid">
+          <div className="row">
+            <div className="col">
+              <p className="h3">
+                <button className="btn mx-2" style={{ backgroundColor: PINK }}>
+                  ساخت مخاطب جدید
+                  <i className="fa fa-plus-circle mx-2" />
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="container">
+        <div className="row">
+          <Contact />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contacts
+export default Contacts;
