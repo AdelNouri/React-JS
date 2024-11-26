f8a4fa6c2d62a1f5ce2f0158029df3906ff6dff3
Adel Nouri <adelnouri0231@gmail.com>
2024-11-26 23:28:00 +0330

    new project to learn hooks

---
+++ react-hooks/src/App.js
@@ -0,0 +1,15 @@
+import UseRefExample from "./hooks/UseRefExample";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="text-center mt-2">
+        <h2>یادگیری و استفاده از هوک های ری اکت 🚀</h2>
+      </div>
+      <hr className="text-danger" />
+      <UseRefExample />
+    </div>
+  );
+};
+
+export default App;
