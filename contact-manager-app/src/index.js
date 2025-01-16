d312752c753c5fd311f63d67386783d4ac05d227
Adel Nouri <adelnouri0231@gmail.com>
2025-01-16 23:28:33 +0330

    Start of Chapter 10 Learning

--- contact-manager-app/src/index.js
+++ contact-manager-app/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {StrictMode} from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
@@ -10,9 +10,9 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
