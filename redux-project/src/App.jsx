2f6cdf40496a58c97635b4f0cd15ca1eade08d39
AdelNouri <adelnouri0231@gmail.com>
2025-09-10 00:35:20 +0330

    Learning Store, Dispatch, Selectors in Redux

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { increment, store, currentValue } from './redux'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,8 +19,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => store.dispatch(increment(2))}>
+          count is {currentValue}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
