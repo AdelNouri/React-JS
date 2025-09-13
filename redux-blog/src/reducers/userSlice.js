209ff57ab52f6f9502f803b9ab66c6e0615043be
AdelNouri <adelnouri0231@gmail.com>
2025-09-13 11:06:35 +0330

    Introduction to createAsyncThunk API

--- redux-blog/src/reducers/userSlice.js
+++ redux-blog/src/reducers/userSlice.js
@@ -1,26 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAllUsers } from "../services/blogsServices";
 
 const initialState = [
-    {
-        id: "1",
-        fullname: "adel nouri"
-    },
-    {
-        id: "2",
-        fullname: "ali nouri"
-    },
-    {
-        id: "3",
-        fullname: "mohammad baghery"
-    }
-]
+  {
+    id: "1",
+    fullname: "adel nouri",
+  },
+  {
+    id: "2",
+    fullname: "ali nouri",
+  },
+  {
+    id: "3",
+    fullname: "mohammad baghery",
+  },
+];
+
+export const fetchAllUsers = createAsyncThunk("/users/fetchUsers", async () => {
+  const response = await getAllUsers();
+
+  return response.data;
+});
 
 const usersSlice = createSlice({
-    name:"users",
-    initialState,
-    reducers: {
+  name: "users",
+  initialState,
+  reducers: {},
+});
 
-    }
-})
-
-export default usersSlice.reducer
+export default usersSlice.reducer;
