69c4105f8c3198a3b3373a5f1116d88cd64f271c
AdelNouri <adelnouri0231@gmail.com>
2025-09-14 23:46:11 +0330

    Store configuration for API

--- redux-blog/src/store/index.js
+++ redux-blog/src/store/index.js
@@ -1,12 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import blogsReducer from "../reducers/blogSlice";
 import usersReducer, { fetchAllUsers } from "../reducers/userSlice";
+import { apiSlice } from "../api/apiSlice";
 
 export const store = configureStore({
   reducer: {
     blogs: blogsReducer,
     users: usersReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 store.dispatch(fetchAllUsers());
