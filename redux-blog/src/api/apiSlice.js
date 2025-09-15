74905d5e0ff74f5b6547d3f92ab85547ce3c7c2c
AdelNouri <adelnouri0231@gmail.com>
2025-09-15 13:00:43 +0330

    Editing a post using RTK Query

--- redux-blog/src/api/apiSlice.js
+++ redux-blog/src/api/apiSlice.js
@@ -1,19 +1,30 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:9000"}),
-    tagTypes: ["BLOG"],
-    endpoints: builder => ({
-        getBlogs: builder.query({query: () => "/blogs", providesTags: ["BLOG"]}),
-        getBlog: builder.query({query: (blogId) => `/blogs/${blogId}`}),
-        addNewBlog: builder.mutation({query: blog => ({
-            url: "/blogs",
-            method: "POST",
-            body: blog 
-        }),
-    invalidatesTags:["BLOG"]})
-    })
-})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  tagTypes: ["BLOG"],
+  endpoints: (builder) => ({
+    getBlogs: builder.query({ query: () => "/blogs", providesTags: ["BLOG"] }),
+    getBlog: builder.query({ query: (blogId) => `/blogs/${blogId}` }),
+    addNewBlog: builder.mutation({
+      query: (blog) => ({
+        url: "/blogs",
+        method: "POST",
+        body: blog,
+      }),
+      invalidatesTags: ["BLOG"],
+    }),
+    editBlog: builder.mutation({
+      query: (blog) => ({
+        url: `/blogs/${blog.id}`,
+        method: "PUT",
+        body: blog,
+      }),
+    //   invalidatesTags: ["BLOG"],
+    }),
+  }),
+});
 
-export const {useGetBlogsQuery, useGetBlogQuery, useAddNewBlogMutation} = apiSlice
+export const { useGetBlogsQuery, useGetBlogQuery, useAddNewBlogMutation, useEditBlogMutation } =
+  apiSlice;
--- redux-blog/src/components/EditBlog.jsx
+++ redux-blog/src/components/EditBlog.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { blogUpdated, editBlog, selectBlogById } from "../reducers/blogSlice";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useEditBlogMutation, useGetBlogQuery } from "../api/apiSlice";
+import Spinner from "./Spinner";
 
 const EditBlog = () => {
   const { blogId } = useParams();
-  const blog = useSelector((state) => selectBlogById(state, blogId));
+  const { data: blog, isFetching, isSuccess, isError } = useGetBlogQuery(blogId);
 
-  const [title, setTitle] = useState(blog.title);
-  const [content, setContent] = useState(blog.content);
+  console.log(blogId);
+  console.log(blog);
 
-  const dispatch = useDispatch();
+  const [updateBlog] = useEditBlogMutation();
+
+  const [title, setTitle] = useState("");
+  const [blogContent, setBlogContent] = useState("");
+
+  useEffect(() => {
+    if (isSuccess) {
+      setTitle(blog.title);
+      setBlogContent(blog.content);
+    }
+  }, [blog]);
+
   const navigate = useNavigate();
 
   const onTitleChange = (e) => {
@@ -18,63 +29,63 @@ const EditBlog = () => {
   };
 
   const onContentChange = (e) => {
-    setContent(e.target.value);
+    setBlogContent(e.target.value);
   };
 
   const handleSubmitForm = () => {
     if (title && content) {
-      dispatch(
-        editBlog({
-          id: blog.id,
-          date: blog.date,
-          title,
-          content,
-          user: blog.user,
-          reactions: blog.reactions,
-        })
-      );
+      updateBlog({
+        id: blog.id,
+        date: blog.date,
+        title,
+        content: blogContent,
+        user: blog.user,
+        reactions: blog.reactions,
+      });
       setTitle("");
-      setContent("");
+      setBlogContent("");
       navigate("/");
     }
   };
 
-  if (!blog) {
-    return (
-      <section>
-        <h2>The post you are looking for does not exist</h2>
-      </section>
+  let content;
+
+  if (isFetching) {
+    content = <Spinner text="Loading..." />;
+  } else if (isSuccess) {
+    content = (
+      <>
+        <h2>Edit blog</h2>
+        <form action="off">
+          <label htmlFor="blogTitle"> blog title: </label>
+          <input
+            type="text"
+            name="blogTitle"
+            id="blogTitle"
+            value={title}
+            onChange={onTitleChange}
+          />
+
+          <label htmlFor="blogContent"> blog content: </label>
+          <textarea
+            name="blogContent"
+            id="blogContent"
+            value={blogContent}
+            onChange={onContentChange}
+          />
+
+          <button type="button" onClick={handleSubmitForm}>
+            {" "}
+            Update blog
+          </button>
+        </form>
+      </>
     );
+  } else if (isError) {
+    content = <h2>The post you are looking for does not exist</h2>;
   }
 
-  return (
-    <section>
-      <h2>Edit blog</h2>
-      <form action="off">
-        <label htmlFor="blogTitle"> blog title: </label>
-        <input
-          type="text"
-          name="blogTitle"
-          id="blogTitle"
-          value={title}
-          onChange={onTitleChange}
-        />
-
-        <label htmlFor="blogContent"> blog content: </label>
-        <textarea
-          name="blogContent"
-          id="blogContent"
-          value={content}
-          onChange={onContentChange}
-        />
-
-        <button type="button" onClick={handleSubmitForm}>
-          {" "}
-          Update blog
-        </button>
-      </form>
-    </section>
-  );
+  return <section>{content}</section>;
 };
 
 export default EditBlog;
