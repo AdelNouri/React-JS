efe8d55c02dc63b165cfd1ebd8ee2d0ca78ccb65
AdelNouri <adelnouri0231@gmail.com>
2025-09-16 20:55:03 +0330

    Introduction to Transform Response AND REMOVE PREV CODES

--- redux-blog/src/reducers/userSlice.js
+++ redux-blog/src/reducers/userSlice.js
@@ -1,82 +1,48 @@
 import {
-  createAsyncThunk,
-  createEntityAdapter,
-  createSelector,
-  createSlice,
-  current,
+    createSlice,
+    createAsyncThunk,
+    createEntityAdapter,
+    createSelector,
 } from "@reduxjs/toolkit";
-
-import { getAllUsers, createUser, removeUser } from "../services/blogsServices";
 import { apiSlice } from "../api/apiSlice";
 
-export const extendedApiSlice = apiSlice.injectEndpoints({
-  endpoints: builder => ({
-    getUsers: builder.query({
-      query: () => "/users"
-    })
-  })
-})
+const userAdaptor = createEntityAdapter();
 
-const emptyUsers = [];
+const initialState = userAdaptor.getInitialState();
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getUsers: builder.query({
+            query: () => "/users",
+            transformResponse: (responseData) => {
+                return userAdaptor.setAll(initialState, responseData);
+            },
+        }),
+    }),
+});
 
 export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select();
 
-export const selectAllUsers = createSelector(
-  selectUsersResult,
-  (usersResult) => usersResult?.data ?? emptyUsers
-);
-
-export const selectUserById = createSelector(
-  selectAllUsers,
-  (state, userId) => userId,
-  (users, userId) => users.find((user) => user.id === userId)
-);
-
-// const userAdapter = createEntityAdapter();
-
-// const initialState = userAdapter.getInitialState();
-
-// export const fetchAllUsers = createAsyncThunk("/users/fetchUsers", async () => {
-//   const response = await getAllUsers();
-//   console.log(response.data);
-//   return response.data;
-// });
-
-// export const addNewUser = createAsyncThunk(
-//   "/users/createNewUser",
-//   async (user) => {
-//     const response = await createUser(user);
-//     return response.data;
-//   }
-// );
-
-// export const deleteUser = createAsyncThunk(
-//   "/users/deleteUser",
-//   async (userId) => {
-//     await removeUser(userId);
-//     return userId;
-//   }
-// );
-
 const usersSlice = createSlice({
-  name: "users",
-  initialState: emptyUsers,
-  reducers: {},
-  // extraReducers: (builder) => {
-  //   builder
-  //     .addCase(
-  //       fetchAllUsers.fulfilled,
-  //       userAdapter.setAll
-  //       // With returning a new result Immer will existing state with whatever we return
-  //     )
-  //     .addCase(addNewUser.fulfilled, userAdapter.addOne)
-  //     .addCase(deleteUser.fulfilled, userAdapter.removeOne);
-  // },
+    name: "users",
+    initialState,
+    reducers: {},
+    // extraReducers: (builder) => {
+    //     builder
+    //         .addCase(fetchUsers.fulfilled, userAdaptor.setAll)
+    //         .addCase(addNewUser.fulfilled, userAdaptor.addOne)
+    //         .addCase(deleteApiUser.fulfilled, userAdaptor.removeOne);
+    // },
 });
 
-// export const { selectAll: selectAllAuthors, selectById: selectAuthorById } =
-//     userAdapter.getSelectors((state) => state.users);
+const selectUsersData = createSelector(
+    selectUsersResult,
+    (usersResult) => usersResult.data
+);
 
-export const {useGetUsersQuery} = extendedApiSlice
+export const { selectAll: selectAllUsers, selectById: selectUserById } =
+    userAdaptor.getSelectors((state) => selectUsersData(state) ?? initialState);
+
+export const { useGetUsersQuery } = extendedApiSlice;
 
 export default usersSlice.reducer;
