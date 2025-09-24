9f2f1235f68e513ad858e06a9565e1a7e80d7923
AdelNouri <adelnouri0231@gmail.com>
2025-09-24 17:34:49 +0330

    Introduction to the Link component

--- Next-JS/my-app/app/page.js
+++ Next-JS/my-app/app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>سلام دوست من</h1>
+      <Link href={{pathname: "/about", query: {name: "adel"}}} >درباره ی عادل</Link>
     </main>
   );
 }
