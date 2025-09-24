f834150131432e809ed29e829e7609b464e178f1
AdelNouri <adelnouri0231@gmail.com>
2025-09-24 10:38:39 +0330

    Converting the React development environment to NextGS

---
+++ counter-2/src/pages/index.js
@@ -0,0 +1,86 @@
+// import { Component } from "react";
+
+// class App extends Component {
+//     state = {
+//         count: 1,
+//         name: 'Adel',
+//         age: 15
+//     }
+
+//     changeState() {
+//         this.setState({ count: 5 })
+//         this.set
+//     }
+
+//     render() {
+//         let { count, name, age } = this.state
+
+//         return (
+//             <div>
+//                 <header>
+//                     <h1>شمارنده ی من</h1>
+//                 </header>
+
+//                 <p>{count}</p>
+//                 <p>{name}</p>
+//                 <p>{age}</p>
+//             </div>
+//         )
+//     }
+// }
+
+//---------------------------------------------------//
+
+import { useState, useCallback } from "react";
+import Counter from "../components/Counter.jsx";
+// import './css/App.css'
+import styles from "./App.module.css";
+import throttle from "lodash.throttle";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const logCount = (count) => {
+    console.log("INCREMENT: " + count);
+  };
+
+  const throttleLog = useCallback(throttle(logCount, 2000), []);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+    throttleLog(count);
+  };
+
+  const decreaseCount = () => {
+    if (count === 0) {
+      return alert("بهتره دیگه منفی نباشیم");
+    }
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  const titleStyle = {
+    color: "aqua",
+    border: "1px solid white",
+    padding: "0 0 10px 0",
+  };
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.Appheader}>
+        <h1 style={titleStyle}>شمارنده ی من</h1>
+      </header>
+      <Counter
+        inc={increaseCount}
+        dec={decreaseCount}
+        rest={resetCount}
+        count={count}
+      />
+    </div>
+  );
+};
+
+export default App;
