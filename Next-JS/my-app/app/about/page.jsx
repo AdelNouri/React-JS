c994c1710f38d34f5a5db51e2d2e74ce41a5cf86
AdelNouri <adelnouri0231@gmail.com>
2025-09-24 20:58:05 +0330

    Introduction to Metadata API

--- Next-JS/my-app/app/about/page.jsx
+++ Next-JS/my-app/app/about/page.jsx
@@ -1,9 +1,20 @@
-const About = () => {
-    return (
-        <div>
-            <h2>درباره ی من</h2>
-        </div>
-    )
+// export const metadata = {
+//   title: {
+//     default: "About",
+//     //  absolute: "About"
+//   },
+// };
+
+export function generateMetadata({params, searchParams}) {
+    return {title:  searchParams.name}
 }
 
-export default About
+const About = () => {
+  return (
+    <div>
+      <h2>درباره ی من</h2>
+    </div>
+  );
+};
+
+export default About;
