54a3b01e4af9584a642253e67b7a84e9f85f65b7
AdelNouri <adelnouri0231@gmail.com>
2025-09-25 21:45:29 +0330

    UI Design Test Application Display Results

--- Next-JS/quiz-app/app/quiz/page.js
+++ Next-JS/quiz-app/app/quiz/page.js
@@ -8,7 +8,7 @@ export default function Page() {
     const [selectedAnswer, setSelectedAnswer] = useState("");
     const [checked, setChecked] = useState(false);
     const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-    const [showResult, setShowResult] = useState(false);
+    const [showResult, setShowResult] = useState(true);
     const [result, setResult] = useState({
         score: 0,
         correctAnswers: 0,
@@ -60,7 +60,18 @@ export default function Page() {
                 ) : (
                     <div className="quiz-container">
                         <h3>نتایج</h3>
-                        {/* Show quiz result */}
+                        <h3>
+                            به طور کلی {(result.score / 25) * 100}% سوالات جواب
+                            داده شده
+                        </h3>
+                        <p>کل سوالات : {questions.length}</p>
+                        <p>کل امتیاز : {result.score}</p>
+                        <p>سوالات درست : {result.correctAnswers}</p>
+                        <p>سوالات غلط : {result.wrongAnswers}</p>
+
+                        <button onClick={() => window.location.reload()}>
+                            شروع مجدد آزمون
+                        </button>
                     </div>
                 )}
             </div>
